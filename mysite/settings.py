6efe237a9e63a49dd60a3f9babd1af94c084d45a
Valerie Wang <1799595877@qq.com>
2016-02-26 10:21:56 +0800

    remove bootstrap admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,12 +40,7 @@ INSTALLED_APPS = [
     'article.apps.ArticleConfig',
 ]
 
-from django.conf import global_settings
-TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + [
-    'django.core.context_processors.request',
-]
 
-BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
